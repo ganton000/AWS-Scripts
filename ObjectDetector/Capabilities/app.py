77ee664e4db95317c77fde10961b04653024376e
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-07-22 17:18:51 -0400

    Updated frontend

--- ObjectDetector/Capabilities/app.py
+++ ObjectDetector/Capabilities/app.py
@@ -1,29 +1,30 @@
 from chalice import Chalice
+from chalicelib import storage_service
+from chalicelib import recognition_service
+import sys
+import random
 
 app = Chalice(app_name='Capabilities')
+app.debug = True
 
+storage_location = "contents.anton.ai"
+storage_service = storage_service.StorageService(storage_location)
+recognition_service = recognition_service.RecognitionService(storage_service)
 
-@app.route('/')
-def index():
-    return {'hello': 'world'}
+@app.route('/demo-object-detection', cors=True)
+def demo_object_detection() -> dict:
+    '''
+    Randomly selects an image for object detection
+    '''
+    files = storage_service.list_files()
+    images = [ file for file in files if file['file_name'].endswith(".jpg") ]
+    image = random.choice(images)
 
+    print('filename:', image['file_name'])
+    objects = recognition_service.detect_objects(image['file_name'])
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+    return {
+        "imageName" : image['file_name'],
+        "imageUrl" : image['url'],
+        "objects" : objects
+    }
--- ObjectDetector/Capabilities/chalicelib/recognition_service.py
+++ ObjectDetector/Capabilities/chalicelib/recognition_service.py
@@ -1,16 +1,18 @@
 import boto3
+from typing import Type
+from .storage_service import StorageService
 
 class RecognitionService:
-	def __init__(self, storage_service) -> None:
+	def __init__(self, storage_service: Type[StorageService]) -> None:
 		self.client = boto3.client('rekognition')
-		self.bucket_name = storage_service
+		self.bucket_name = storage_service.get_storage_location()
 
-	def detect_objects(self, file_name) -> list[dict]:
+	def detect_objects(self, file_name: str) -> list[dict]:
 		response = self.client.detect_labels(
 			Image = {
-				'S3Object': {
-					'Bucket': self.bucket_name,
-					'Name': file_name
+				"S3Object": {
+					"Bucket": self.bucket_name,
+					"Name": file_name
 				}
 			}
 		)
@@ -18,8 +20,8 @@ class RecognitionService:
 		objects = []
 		for label in response["Labels"]:
 			objects.append({
-				'label': label['Name'],
-				'confidence': label['Confidence']
+				"label": label['Name'],
+				"confidence": label['Confidence']
 			})
 
 		return objects

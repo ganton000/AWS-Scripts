5c66215f266df44e176ef94d7fa41e3a0a33499e
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-07-25 10:52:21 -0400

    Added functions for script.js

--- PictorialTranslator/backend/app.py
+++ PictorialTranslator/backend/app.py
@@ -1,29 +1,54 @@
 from chalice import Chalice
+from chalicelib import storage_service
+from chalicelib import recognition_service
+from chalicelib import translation_service
+
+import json
+import base64
 
 app = Chalice(app_name='backend')
+app.debug = True
 
 
-@app.route('/')
-def index():
-    return {'hello': 'world'}
+#Init
+storage_location = 'contents.anton.ai'
+storage_service = storage_service.StorageService(storage_location)
+recognition_service = recognition_service.RecognitionService(storage_service)
+translation_service = translation_service.TranslationService()
 
+#REST Endpoints
+@app.route('/images/{image_id}/translated-text', methods=['POST'], cors=True)
+def translate_image_text(image_id):
+    '''
+    detects and then translates the text within a specified image
+    '''
+    request_data = json.loads(app.current_request.raw_body)
+    from_lang = request_data['fromLang']
+    to_lang = request_data['toLang']
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+    MIN_CONFIDENCE = 80.0
+
+    text_lines = recognition_service.detect_text(image_id)
+
+    translated_lines = []
+    for line in text_lines:
+        if float(line['confidence']) >= MIN_CONFIDENCE:
+            translated_line = translation_service.translate_text(line['text'], from_lang, to_lang)
+            translated_lines.append({
+                'text': line['text'],
+                'translation': translated_line,
+                'boundingBox': line['boundingBox']
+            })
+
+    return translated_lines
+
+@app.route('/images', methods=['POST'], cors=True)
+def upload_image():
+    """processes file upload and saves file to storage service"""
+    request_data = json.loads(app.current_request.raw_body)
+    file_name = request_data['filename']
+    file_bytes = base64.b64decode(request_data['filebytes'])
+
+    image_info = storage_service.upload_file(file_bytes, file_name)
+
+    return image_info

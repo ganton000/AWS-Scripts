c5e1ac5522d83e01c3d477647718d7fd2fb8e230
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-07-26 11:44:07 -0400

    Added routes for each respective action

--- ContactOrganizer/backend/app.py
+++ ContactOrganizer/backend/app.py
@@ -1,29 +1,69 @@
 from chalice import Chalice
+from chalicelib import storage_service
+from chalicelib import recognition_service
+from chalicelib import extraction_service
+from chalicelib import contact_store
 
-app = Chalice(app_name='backend')
+import base64
+import json
+
+app = Chalice(app_name='Capabilities')
+app.debug = True
+
+#services initialization
+storage_location = 'contents.anton.ai'
+storage_service = storage_service.StorageService(storage_location)
+recognition_service = recognition_service.RecognitionService(storage_location)
+extraction_service = extraction_service.ExtractionService()
+store_location = 'Contacts'
+contact_store = contact_store.ContactStore(store_location)
+
+#RESTful endpoints
+@app.route('/images/{image_id}/extract-info', methods=['POST'], cors=True)
+def extract_image_info(image_id):
+    '''
+    Detects text in the specified image
+    Then extracts contact information from the text
+    '''
+    MIN_CONFIDENCE = 70.0
+
+    text_lines = recognition_service.detect_text(image_id)
+
+    contact_lines = []
+    for line in text_lines:
+        #check confidence
+        if float(line['confidence']) >= MIN_CONFIDENCE:
+            contact_lines.append(line['text'])
+
+        contact_string = '  '.join(contact_lines)
+        contact_info = extraction_service.extract_contact_info(contact_string)
+
+    return contact_info
+
+@app.route('/contacts', methods = ['POST'], cors = True)
+def save_contact():
+    """saves contact information to the contact store service"""
+    request_data = json.loads(app.current_request.raw_body)
+
+    contact = contact_store.save_contact(request_data)
+
+    return contact
 
 
-@app.route('/')
-def index():
-    return {'hello': 'world'}
+@app.route('/contacts', methods = ['GET'], cors = True)
+def get_all_contacts():
+    """gets all saved contacts in the contact store service"""
+    contacts = contact_store.get_all_contacts()
 
+    return contacts
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+@app.route('/images', methods = ['POST'], cors = True)
+def upload_image():
+    """processes file upload and saves file to storage service"""
+    request_data = json.loads(app.current_request.raw_body)
+    file_name = request_data['filename']
+    file_bytes = base64.b64decode(request_data['filebytes'])
+
+    image_info = storage_service.upload_file(file_bytes, file_name)
+
+    return image_info

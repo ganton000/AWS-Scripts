b3b30b1b02b851ad25f0723f766817af636513fd
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-07-28 17:44:01 -0400

    Script to get ebs volumes and create snapshot for ec2

---
+++ boto3_scripts/src/lambda_pkg/ebs_snapshot_automation_with_lambda_cloudwatch.py
@@ -0,0 +1,58 @@
+import boto3
+
+profileName = "default"
+session = boto3.session.Session(profile_name=profileName)
+
+ec2_client = boto3.client("ec2", "us-east-1")
+
+filter_prod_backup = {
+		'Name': 'tag:Prod',
+		'Values': ['backup', 'Backup']
+	}
+
+def list_all_ebs_volumes_with_filter(filter):
+
+	vol_iter = ec2_client.describe_volumes(Filters=[filter])['Volumes']
+
+	if len(vol_iter) == 0: return None
+	else:
+		list_of_vol_ids= []
+		for each_vol in vol_iter:
+			list_of_vol_ids.append(each_vol['VolumeId'])
+
+	return list_of_vol_ids
+
+
+#list_all_ebs_volumes_with_filter(filter_prod_backup)
+
+def paginate_over_ebs_volumes_with_filter(filter):
+	paginator = ec2_client.get_paginator("describe_volumes")
+	for each_page in paginator.paginate(Filters=[filter]):
+
+		if len(each_page['Volumes']) == 0: return None
+		else:
+			list_of_vol_ids= []
+
+			for each_vol in each_page['Volumes']:
+				list_of_vol_ids.append(each_vol['VolumeId'])
+
+	return list_of_vol_ids
+
+list_of_volids = paginate_over_ebs_volumes_with_filter(filter_prod_backup)
+
+def generate_snapshot_of_volid(volids: list):
+	for each_volid in list_of_volids:
+		print(f"Taking snap of {each_volid}")
+		ec2_client.create_snapshot(
+			Description="Taking snap with Lambda and CW",
+			VolumeId=each_volid,
+			TagSpecifications=[{
+				'ResourceType': 'snapshot',
+				'Tags': [{
+					'Key': 'Delete-on',
+					'Value': '90-days'
+				}]
+			}]
+		)
+
+generate_snapshot_of_volid(list_of_volids)

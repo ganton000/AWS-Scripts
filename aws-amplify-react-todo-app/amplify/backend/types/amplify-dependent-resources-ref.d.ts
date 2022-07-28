08425cf1852a62d28a87afd1c7eb3aaf116e81ef
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-07-27 20:19:15 -0400

    Made some changes in app.tsx and config files

---
+++ aws-amplify-react-todo-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "reactamplifytodoapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- aws-amplify-react-todo-app/src/App.tsx
+++ aws-amplify-react-todo-app/src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Amplify, API, graphqlOperation } from "aws-amplify";
+import { createTodo } from "./graphql/mutations";
+import { listTodos } from "./graphql/queries";
+import awsExports from "./aws-exports";
+
+import "./App.css";
+
+//configures Amplify with AWS credentials
+Amplify.configure(awsExports);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <div>App</div>;
 }
 
 export default App;

9a74e2889bf193906daa8b0d0cc96c2c58dd6f74
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-08-01 19:50:35 -0400

    set up boilerplate for graphql-express project

---
+++ aws-graphql/server/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+	graphiql: true
+}))
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+    console.log(`Listening in on PORT: ${PORT}`);
+});

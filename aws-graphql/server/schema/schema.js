2a69465354b94b18888d2f2e21d80e660c0b0a01
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-08-01 22:07:03 -0400

    Added another Model type

--- aws-graphql/server/schema/schema.js
+++ aws-graphql/server/schema/schema.js
@@ -6,19 +6,68 @@ const {
     GraphQLSchema,
 } = require("graphql");
 
+let _ = require("lodash");
+
+let usersData = [
+    {
+        id: "345",
+        name: "Anton",
+        age: 32,
+        profession: "Engineer",
+    },
+    {
+        id: "123",
+        name: "Lobert",
+        age: 43,
+        profession: "Teacher",
+    },
+];
+
+let hobbiesData = [
+    {
+        id: "1",
+        title: "Programming",
+        description: "Using computers",
+    },
+    {
+        id: "2",
+        title: "Fencing",
+        description: "Using swords",
+    },
+];
+
 //Types
 const UserType = new GraphQLObjectType({
     name: "User",
     description: "Documentation for User...",
     fields: () => ({
         id: {
-            type: GraphQLString,
+            type: GraphQLID,
         },
         name: {
             type: GraphQLString,
         },
         age: {
             type: GraphQLInt,
+        },
+        profession: {
+            type: GraphQLString,
+        },
+    }),
+});
+
+const HobbyType = new GraphQLObjectType({
+    name: "Hobby",
+    description: "Hobby description",
+    fields: () => ({
+        id: {
+            type: GraphQLID,
+        },
+        title: {
+            type: GraphQLString,
+        },
+        description: {
+            type: GraphQLString,
         },
     }),
 });
@@ -30,10 +79,21 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         user: {
             type: UserType,
-            args: { id: { type: GraphQLString } },
+            args: { id: { type: GraphQLID } },
 
             resolve(parent, args) {
                 //get and return data from data source
+
+                return _.find(usersData, { id: args.id });
+            },
+        },
+        hobby: {
+            type: HobbyType,
+            args: { id: { type: GraphQLID } },
+
+            resolve(parent, args) {
+
+                return _.find(hobbiesData, { id: args.id });
             },
         },
     },

66fe256ac7a4544ac62bf1b8f073d705d74baf83
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-08-07 21:19:09 -0400

    Added python CDK stack which deploys API Gateway, lambda and s3

--- pycdkworkshop/pycdkworkshop/pycdkworkshop_stack.py
+++ pycdkworkshop/pycdkworkshop/pycdkworkshop_stack.py
@@ -3,7 +3,10 @@ from aws_cdk import (
     Duration,
     Stack,
     aws_iam as iam,
+    aws_s3 as s3,
     aws_sqs as sqs,
+    aws_apigateway as api,
+    aws_lambda as _lambda,
     aws_sns as sns,
     aws_sns_subscriptions as subs,
 )
@@ -21,6 +24,29 @@ class PycdkworkshopStack(Stack):
 
         topic = sns.Topic(
             self, "PycdkworkshopTopic"
+        )
+
+        bucket = s3.Bucket(
+            self,
+            id='s3cdkbucket',
+            bucket_name='examplecdkbucket',
+            versioned=True
+        )
+
+        lambdaFunction = _lambda.Function(
+            self,
+            id="lambdafunction",
+            code= _lambda.Code.from_asset(
+                path='lambdacode'
+            ),
+            handler= 'hello.handler',
+            runtime= _lambda.Runtime.PYTHON_3_9
+        )
+
+        lambdaApi = api.LambdaRestApi(
+            self,
+            id='restapi',
+            handler=lambdaFunction
         )
 
         topic.add_subscription(subs.SqsSubscription(queue))
